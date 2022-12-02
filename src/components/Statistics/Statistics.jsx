9cda5eb1766ad20fe85944cbc7f470c175e11880
Alina Romanova <alinakhraban@gmail.com>
2022-12-02 13:11:55 +0200

    edits

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,3 +1,32 @@
+import React from 'react';
+import css from '../Statistics/Statistics.styles.css'
+import Stats from "../Stats/Stats";
+import PropTypes from "prop-types";
+
+const Statistics = ({title, stats}) => {
+
+  return (
+    <section className="statistics">
+  {title && <h2 className="title">{title}</h2>}
+
+  <ul className="stat-list">
+  {stats.map((stat) => (
+        <Stats
+        key={stat.id} stat={stat}/>
+    ))}
+  </ul>
+</section>
+  )
+}
+
+export default Statistics;
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired
+}).isRequired).isRequired,
+}
 
 
 
