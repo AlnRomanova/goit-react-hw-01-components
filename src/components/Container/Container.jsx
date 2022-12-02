4664f6de71a5b3b74ba601905d430ff1a06045e2
Alina Romanova <alinakhraban@gmail.com>
2022-12-03 00:31:36 +0200

    add propType

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,8 +1,14 @@
-import css from './Container.module.css'
+import css from './Container.module.css';
+import PropTypes from "prop-types";
 
 const Container = ({ children }) => {
+  console.log(children)
   return <div className={css.container}>{children}</div>;
 };
 
 export default Container;
+
+Container.propTypes = {
+children: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+}
 
