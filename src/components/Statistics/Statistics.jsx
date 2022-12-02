cdbe0b00542ce6e47191fb529ad236af51283130
Alina Romanova <alinakhraban@gmail.com>
2022-12-02 14:32:49 +0200

    edits

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../Statistics/Statistics.styles.css'
+import './Statistics.styles.css'
 import Stats from "../Stats/Stats";
 import PropTypes from "prop-types";
 
